a677b95f073a62d07f31c9e7d81077ef466e7fa4
ctoLu <2296663099@qq.com>
2024-05-05 16:52:03 +0800

    solve heap overflow

--- Classification/KNN/main.cpp
+++ Classification/KNN/main.cpp
@@ -6,10 +6,10 @@ size_t testSize = (size_t)(SIZE * (1 - TRAIN_RATE));
 
 int main()
 {
-	Iris* irises, * train, * test;
+	Iris* irises, * train;
 	irises = (Iris*)malloc(SIZE * sizeof(Iris));
 	train = (Iris*)malloc(trainSize * sizeof(Iris));
-	test = (Iris*)malloc(testSize * sizeof(Iris));
+	Iris test[30];
 
 	if (!readTxt(irises))
 		return 0;
@@ -18,4 +18,5 @@ int main()
 	// ÊÍ·ÅÄÚ´æ
 	free(irises);
 	predict(train, test, 100);
+	free(train);
 }
